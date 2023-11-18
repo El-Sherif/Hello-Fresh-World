1514c00905316add34c2df367ba2b8b7bc306040
El-Sherif <ahmedelsherif98j@gmail.com>
2023-11-18 16:14:46 +0100

    Update text_generation.py

--- streamlit/text_generation.py
+++ streamlit/text_generation.py
@@ -1,19 +1,24 @@
-import openai
+from openai import OpenAI
+client = OpenAI()
 from prompt_store import get_prompt_1
 
 def generate_recipe(age, gender, mood):
     # Your OpenAI API key
-    openai.api_key = ''
+    client = OpenAI(api_key='key')
 
     # Constructing the prompt
     prompt_text = get_prompt_1(age, gender, mood)
 
-    response = openai.ChatCompletion.create(
-      engine="gpt-4-1106-preview", # Or the latest available model
-      message=prompt_text,
-    )
+    response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": f"{prompt_text}"}
+  ]
+)
 
-    return response.choices[0].message['content']
+    return response.choices[0].message.content
+
 
 
 # Example usage
